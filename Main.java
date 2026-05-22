bc8de2b368d65538ceabe6f8a09ad906fa71fbd6
arvjcs-netizen <arvjcs@gmail.com>
2026-05-21 17:20:45 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.awt.*;
-import javax.swing.JFrame;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import javax.swing.JFrame;
 
 public class Main extends Canvas implements KeyListener{
     
